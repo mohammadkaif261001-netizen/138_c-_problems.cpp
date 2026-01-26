2d1bc17ce27d6ce859400a83bfd77898eac11d2e
Kaif <mohammadkaif261001@gmail.com>
2026-01-26 21:54:57 +0600

    problem-06.cpp

---
+++ problem_06.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int num1, num2, temp;
+    cout<<"Enter two numbers: ";
+    cin>>num1>>num2;
+    temp=num1;
+    num1=num2;
+    num2=temp;
+    cout<<"After swaping: ";
+    cout<<"First number:"<<num1<<endl;
+    cout<<"Second number: "<<num2<<endl;
+    return 0;
+
+}
