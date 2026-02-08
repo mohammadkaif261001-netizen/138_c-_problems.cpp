79da9d82508624178fb393de2326e219863d54ac
Kaif <mohammadkaif261001@gmail.com>
2026-02-08 22:44:35 +0600

    problem_13.cpp

---
+++ problem_13.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    char ch; 
+    cout<<"A to Z alphabets: "<<endl;
+    for(ch='A'; ch<='Z'; ch++){
+        cout<<ch<<endl;
+    }
+    return 0;
+}
